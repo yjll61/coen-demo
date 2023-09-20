bdfc1117ed6e44007acf070184d72720416b80a4
umroot <umroot@CONCORD-89KG62T.encs.concordia.ca>
2023-09-20 14:21:38 -0400

    change prompt message

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from google.cloud import bigquery
 def hello_world():
     """Example Hello World route."""
 
-    return f"Hello World!!!@@@!!!"
+    return f"Welcome to Programming on Cloud!"
 
 @app.route("/event_looks", methods=['POST'])
 def event_looks():
